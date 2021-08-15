4e4d3e364a695b686c1d26fb58d7926e58eef978
Danny Shen <shenfive@hotmail.com>
2021-08-15 15:53:10 +0800

    kf

--- A815HelloNetImage/ViewController.swift
+++ A815HelloNetImage/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
     
@@ -13,26 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var theImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
-        
-        
-        
-        
         let start = Date().timeIntervalSince1970
-        let imageAddress = "https://shop.r10s.jp/aikimania/cabinet/new/sw-1758_sp.jpg"
+        let imageAddress = "https://www.mirrormedia.com.tw/assets/images/20200515182647-44d2dc790fce36e05cd131b09da09bd6-mobile.jpg"
         if let imageURL = URL(string: imageAddress){
-            DispatchQueue.global().async {
-                do{
-                    let imageData = try Data(contentsOf: imageURL)
-                    DispatchQueue.main.async {
-                        self.theImageView.image = UIImage(data: imageData)
-                        print("loded:\(Date().timeIntervalSince1970 - start)")
-                    }
-                }catch{
-                    print(error.localizedDescription)
-                }
-            }
+//            DispatchQueue.global().async {
+//                do{
+//                    let imageData = try Data(contentsOf: imageURL)
+//                    DispatchQueue.main.async {
+//                        self.theImageView.image = UIImage(data: imageData)
+//                        print("loded:\(Date().timeIntervalSince1970 - start)")
+//                    }
+//                }catch{
+//                    print(error.localizedDescription)
+//                }
+//            }
+            
+            theImageView.kf.setImage(with: imageURL)
+            
             print("next:\(Date().timeIntervalSince1970 - start)")
         }
     }
@@ -43,16 +42,14 @@ class ViewController: UIViewController {
         
         theImageView.clipsToBounds = true
         theImageView.layer.cornerRadius = theImageView.frame.size.height / 2
-        theImageView.layer.borderColor = UIColor.red.cgColor
+        theImageView.layer.borderColor = UIColor.black.cgColor
         theImageView.layer.borderWidth = 1
         
         contaner.layer.cornerRadius = theImageView.frame.size.height / 2
-        contaner.layer.borderColor = UIColor.red.cgColor
-        contaner.layer.borderWidth = 1
         
         contaner.clipsToBounds = false
         contaner.layer.shadowRadius = 20
-        contaner.layer.shadowColor = UIColor.green.cgColor
+        contaner.layer.shadowColor = UIColor.gray.cgColor
         contaner.layer.shadowOffset = CGSize(width: 10, height: 10)
         contaner.layer.shadowOpacity = 0.6
     }
