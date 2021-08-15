a02fc458d0094fbeb3baa065d1268f9c10fc3301
Danny Shen <shenfive@hotmail.com>
2021-08-15 13:36:52 +0800

    load Image

--- A815HelloNetImage/ViewController.swift
+++ A815HelloNetImage/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var theImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let imageAddress = "https://shop.r10s.jp/aikimania/cabinet/new/sw-1758_sp.jpg"
+        if let imageURL = URL(string: imageAddress){
+            do{
+                let imageData = try Data(contentsOf: imageURL)
+                theImageView.image = UIImage(data: imageData)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
     }
-
+    
 
 }
 
