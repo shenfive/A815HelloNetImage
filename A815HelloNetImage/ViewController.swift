b56dc3ca45b6745d21b389223a72c740c6a422c5
Danny Shen <shenfive@hotmail.com>
2021-08-15 14:44:56 +0800

    show2

--- A815HelloNetImage/ViewController.swift
+++ A815HelloNetImage/ViewController.swift
@@ -38,8 +38,8 @@ class ViewController: UIViewController {
     }
 
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated )
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
         theImageView.clipsToBounds = true
         theImageView.layer.cornerRadius = theImageView.frame.size.height / 2
