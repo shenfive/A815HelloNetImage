4fdc08866aff4977e6db043660a95e92c325dd84
Danny Shen <shenfive@hotmail.com>
2021-08-15 13:56:16 +0800

    time

--- A815HelloNetImage/ViewController.swift
+++ A815HelloNetImage/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var theImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let start = Date().timeIntervalSince1970
         let imageAddress = "https://shop.r10s.jp/aikimania/cabinet/new/sw-1758_sp.jpg"
         if let imageURL = URL(string: imageAddress){
             DispatchQueue.global().async {
@@ -19,11 +20,13 @@ class ViewController: UIViewController {
                     let imageData = try Data(contentsOf: imageURL)
                     DispatchQueue.main.async {
                         self.theImageView.image = UIImage(data: imageData)
+                        print("loded:\(Date().timeIntervalSince1970 - start)")
                     }
                 }catch{
                     print(error.localizedDescription)
                 }
             }
+            print("next:\(Date().timeIntervalSince1970 - start)")
         }
     }
     
